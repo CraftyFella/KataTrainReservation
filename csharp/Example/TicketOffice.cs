f40cc0d0aa7c0327882778fa5d8044bce9a8870b
Craftyfella <Craftyfella@gmail.com>
2013-10-25 10:34:49 +0100

    Dave & Paul Setup a solution and dlls

---
+++ csharp/Example/TicketOffice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Example
+{
+    public class TicketOffice
+    {
+        
+        public Reservation MakeReservation(ReservationRequest request)
+        {
+            //TODO: implement this code!
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ csharp/Example/TicketOfficeNUnitTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Example
+{
+    [TestFixture]
+    public class TicketOfficeNUnitTest {
+    
+        [Test]
+        public void reserveSeats() {
+            // TODO: write this code
+
+        }
+    }
+}
